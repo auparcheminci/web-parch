5b23965094b54f1f2c99f29a8b1839aa0dc40021
ineszarka <zarkaines@gmail.com>
2026-03-04 14:23:41 +0100

    Make Sass available to extranet project

--- apps/extranet/vite.config.ts
+++ apps/extranet/vite.config.ts
@@ -1,5 +1,14 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@acme/styles/index.scss" as *;`,
+      },
+    },
+  },
+});
