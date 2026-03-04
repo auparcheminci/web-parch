5bc5a1dd4ba0718967a58081823077be2cf6883c
ineszarka <zarkaines@gmail.com>
2026-03-04 14:26:26 +0100

    Make Sass available to Front Web project

--- apps/frontweb/vite.config.ts
+++ apps/frontweb/vite.config.ts
@@ -1,5 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+export default defineConfig({
+  plugins: [tailwindcss(), sveltekit()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@acme/styles/index.scss" as *;`,
+      },
+    },
+  },
+});
