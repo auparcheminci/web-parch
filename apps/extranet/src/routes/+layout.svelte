b57501ecb9d477192b97a609e231397865d91ec4
ineszarka <zarkaines@gmail.com>
2026-04-16 10:32:36 +0200

    Amplify Init for Extranet

--- apps/extranet/src/routes/+layout.svelte
+++ apps/extranet/src/routes/+layout.svelte
@@ -2,8 +2,6 @@
 	import './layout.scss';
 	import favicon from '$lib/assets/favicon.svg';
 	let { children } = $props();
-  import '$lib/amplify';
-  import '@aws-amplify/ui-svelte/styles.css';
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
--- apps/extranet/src/routes/+page.svelte
+++ apps/extranet/src/routes/+page.svelte
@@ -1,17 +1,6 @@
 <script lang="ts">
-  
-  import { Authenticator } from "@aws-amplify/ui-svelte";
-  import "@aws-amplify/ui-svelte/styles.css";
-
 </script>
 
 <main class="login">
   <h1>Sign in</h1>
-
-  <Authenticator initialState="signIn" loginMechanisms={['email']}>
-  {#snippet children({ user, signOut })}
-    <h1>Hello {user?.username}</h1>
-    <button onclick={signOut}>Sign out</button>
-  {/snippet}
-</Authenticator>
 </main>
