d75ca08aee34a27112d5a37dbd993b4e97d0eed3
ineszarka <zarkaines@gmail.com>
2026-06-17 15:22:39 +0200

    Grid margin for Extranet Homepage

--- apps/extranet/src/routes/+page.svelte
+++ apps/extranet/src/routes/+page.svelte
@@ -4,24 +4,28 @@
   import type { AuthMachineState } from "@aws-amplify/ui";
 </script>
 
-<div id="home-container">
-  <Authenticator>
-    {#snippet children({
-      signOut,
-      user,
-    }: {
-      signOut: () => void;
-      user: AuthUser;
-      state: AuthMachineState;
-      send: unknown;
-    })}
-      <main>
-        <p>Welcome, Welcome {user?.signInDetails?.loginId}</p>
-        <button onclick={signOut}>Sign out</button>
-      </main>
-    {/snippet}
-  </Authenticator>
-  <div id="other">
+<div id="home-container" class="grid grid-cols-2">
+  <div id="home-col-left" class="grid py-5 pl-5 pr-2.5 min-w-0">
     <p>Bonjour</p>
+  </div>
+  <div id="home-col-right" class="grid py-5 pl-2.5 pr-5 min-w-0">
+    <div id="other">
+      <Authenticator>
+        {#snippet children({
+          signOut,
+          user,
+        }: {
+          signOut: () => void;
+          user: AuthUser;
+          state: AuthMachineState;
+          send: unknown;
+        })}
+          <main>
+            <p>Welcome, Welcome {user?.signInDetails?.loginId}</p>
+            <button onclick={signOut}>Sign out</button>
+          </main>
+        {/snippet}
+      </Authenticator>
+    </div>
   </div>
 </div>
