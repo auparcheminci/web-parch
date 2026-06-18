554f3e3436c82b0698e6e74ebed1bad737e2ff55
ineszarka <zarkaines@gmail.com>
2026-06-18 12:00:47 +0200

    CSS beginning for amplify auth

--- apps/extranet/src/routes/+page.svelte
+++ apps/extranet/src/routes/+page.svelte
@@ -2,6 +2,7 @@
   import { Authenticator } from "@aws-amplify/ui-svelte";
   import type { AuthUser } from "@aws-amplify/auth";
   import type { AuthMachineState } from "@aws-amplify/ui";
+  import './auth.scss';
 </script>
 
 <div id="home-container" class="flex flex-1 overflow-hidden">
@@ -9,7 +10,7 @@
     <img src="https://strapi-z4iu.onrender.com/uploads/1000x1000_100_5cbae1b054.jpg" alt="placeholder" class="object-cover w-full h-full rounded-2xl" />
   </div>
   <div id="home-col-right" class="flex-1 py-5 pl-2.5 pr-5 overflow-hidden">
-    <div id="auth-container" class="w-full h-full">
+    <div id="auth-container" class="w-full h-full flex items-center justify-center">
       <Authenticator>
         {#snippet children({
           signOut,
