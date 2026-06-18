b625a46e39691133bc38d0752a39d55f3dda8c8b
ineszarka <zarkaines@gmail.com>
2026-06-18 16:01:51 +0200

    Amplify form Customization

--- apps/extranet/src/routes/+page.svelte
+++ apps/extranet/src/routes/+page.svelte
@@ -2,39 +2,50 @@
   import { Authenticator } from "@aws-amplify/ui-svelte";
   import type { AuthUser } from "@aws-amplify/auth";
   import type { AuthMachineState } from "@aws-amplify/ui";
-  import './auth.scss';
+  import "./auth.scss";
 </script>
 
 <div id="home-container" class="flex flex-1 overflow-hidden">
   <div id="home-col-left" class="flex-1 py-5 pl-5 pr-2.5 overflow-hidden">
-    <img src="https://strapi-z4iu.onrender.com/uploads/1000x1000_100_5cbae1b054.jpg" alt="placeholder" class="object-cover w-full h-full rounded-2xl" />
+    <img
+      src="https://strapi-z4iu.onrender.com/uploads/1000x1000_100_5cbae1b054.jpg"
+      alt="placeholder"
+      class="object-cover w-full h-full rounded-2xl"
+    />
   </div>
   <div id="home-col-right" class="flex-1 py-5 pl-2.5 pr-5 overflow-hidden">
-    <div id="auth-container" class="w-full h-full flex items-center justify-center">
-      <div id="auth-form-wrapper" class="flex flex-col w-full items-center">
+    <div
+      id="auth-container"
+      class="w-full h-full flex items-center justify-center"
+    >
+      <div id="auth-form-wrapper" class="flex flex-col w-full max-w-md items-center">
         <div id="auth-header">
           <img src="" alt="logo" />
-          <p>Texte d'intro</p>
+          <p>
+            Pour accéder à la plateforme, saisissez votre identifiant et votre
+            mot de passe dans les champs ci-dessous. Si vous ne possédez pas
+            encore de compte, vous pouvez en créer un en cliquant sur “Créer mon
+            compte”.
+          </p>
           <h2>Connexion</h2>
         </div>
         <Authenticator>
-        {#snippet children({
-          signOut,
-          user,
-        }: {
-          signOut: () => void;
-          user: AuthUser;
-          state: AuthMachineState;
-          send: unknown;
-        })}
-          <main>
-            <p>Welcome, Welcome {user?.signInDetails?.loginId}</p>
-            <button onclick={signOut}>Sign out</button>
-          </main>
-        {/snippet}
-      </Authenticator>
+          {#snippet children({
+            signOut,
+            user,
+          }: {
+            signOut: () => void;
+            user: AuthUser;
+            state: AuthMachineState;
+            send: unknown;
+          })}
+            <main>
+              <p>Welcome, Welcome {user?.signInDetails?.loginId}</p>
+              <button onclick={signOut}>Sign out</button>
+            </main>
+          {/snippet}
+        </Authenticator>
       </div>
     </div>
   </div>
 </div>
-
