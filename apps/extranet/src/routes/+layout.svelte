9f63ea9b61288c68ffe39ab156038fb795ce9ef4
ineszarka <zarkaines@gmail.com>
2026-06-19 11:31:49 +0200

    Change Buttons to FR

--- apps/extranet/src/routes/+layout.svelte
+++ apps/extranet/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Amplify } from 'aws-amplify';
+	import { I18n } from 'aws-amplify/utils';
 	import outputs from '../../amplify_outputs.json';
 	import '../app.css';
 	import './layout.scss';
@@ -7,6 +8,22 @@
 	import '@aws-amplify/ui-svelte/styles.css';
 
 	Amplify.configure(outputs, { ssr: true });
+
+	I18n.putVocabulariesForLanguage('en', {
+		// Boutons principaux
+		'Sign In': 'Me connecter',
+		'Sign in': 'Me connecter',
+		'Create Account': 'Créer mon compte',
+		'Confirm': 'Confirmer',
+		'Submit': 'Valider',
+		'Send code': 'Envoyer le code',
+
+		// Liens et actions secondaires
+		'Forgot your password?': 'Mot de passe oublié ?',
+		'Back to Sign In': 'Retour à la connexion',
+		'Resend Code': 'Renvoyer le code',
+		'Reset Password': 'Réinitialiser le mot de passe',
+	});
 
 	let { children } = $props();
 </script>
