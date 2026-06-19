e0afd56007a30d9dd68c5fae8ae95f87b48dd30e
ineszarka <zarkaines@gmail.com>
2026-06-19 10:55:34 +0200

    Back to website button on login page

--- apps/extranet/src/routes/+page.svelte
+++ apps/extranet/src/routes/+page.svelte
@@ -16,8 +16,20 @@
   <div id="home-col-right" class="flex-1 py-5 pl-2.5 pr-5 overflow-hidden">
     <div
       id="auth-container"
-      class="w-full h-full flex items-center justify-center"
+      class="relative w-full h-full flex items-center justify-center"
     >
+      <a
+        id="auth-back-btn"
+        href="https://www.auparchemin.ci/"
+        class="absolute top-0 left-0 p-2.5 rounded-br-xl"
+        aria-label="Retour au site"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
+          <path d="M19 12H5" />
+          <path d="M12 19l-7-7 7-7" />
+        </svg>
+        <span class="auth-back-label">Retour au site</span>
+      </a>
       <div id="auth-form-wrapper" class="flex flex-col w-full max-w-md">
         <div id="auth-header" class="flex flex-col items-center">
           <img src="" alt="logo" />
