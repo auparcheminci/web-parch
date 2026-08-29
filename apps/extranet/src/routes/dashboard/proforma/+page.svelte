d078697e6f28220cb3b0db5366c4263db27362c0
ineszarka <zarkaines@gmail.com>
2026-08-29 22:06:06 +0200

    Cleaning Layout Div Number

--- apps/extranet/src/routes/dashboard/proforma/+page.svelte
+++ apps/extranet/src/routes/dashboard/proforma/+page.svelte
@@ -1,14 +1,8 @@
-<main>
-	 <div class="main-content-space flex-1 min-h-0 p-2.5">
-      <div
-        class="content-template-space flex flex-col justify-between items-start gap-1.25 w-full h-full"
-      >
-        <main class="main-pub-space w-full self-stretch min-h-0">
-          <img
-            alt="Contenu à venir"
-            class="main-pub-picture w-full h-full rounded-lg object-cover bg-gray-200"
-          />
-        </main>
-      </div>
-    </div>
-</main>
+<div
+  class="content-template-space flex flex-col justify-between items-start gap-1.25 w-full h-full"
+>
+  <img
+    alt="Contenu à venir"
+    class="main-pub-picture w-full h-full rounded-lg object-cover bg-gray-200"
+  />
+</div>
