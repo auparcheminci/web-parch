42741dae7047d8f4a98ae04d9c96f98df6e53af0
ineszarka <zarkaines@gmail.com>
2026-09-07 16:31:27 +0200

    Big Layout of Home

--- apps/extranet/src/routes/dashboard/+page.svelte
+++ apps/extranet/src/routes/dashboard/+page.svelte
@@ -20,8 +20,12 @@
 </script>
 
 {#if !checkingAuth}
-  <main>
-    <p>Hello</p>
-   <!-- <ConnectedArticles {user} /> --> 
+  <main class="flex flex-col md:flex-row gap-5 h-full">
+    <section class="grid-section-one w-full md:w-[70%] flex-1 md:flex-none"><p>Section 1</p></section>
+    <section class="grid-section-two w-full md:w-[30%] flex flex-col gap-5 flex-1 md:flex-none">
+      <div class="grid-item-one w-full flex-1"><p>Under one</p></div>
+      <div class="grid-item-two w-full flex-1"><p>Under two</p></div>
+    </section>
+    <!-- <ConnectedArticles {user} /> -->
   </main>
 {/if}
