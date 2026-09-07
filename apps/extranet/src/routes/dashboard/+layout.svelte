830c98ec84d3dd0c6d7e5ae10f4047154459fd15
ineszarka <zarkaines@gmail.com>
2026-09-07 17:12:14 +0200

    Basic Design of the notification bar

--- apps/extranet/src/routes/dashboard/+layout.svelte
+++ apps/extranet/src/routes/dashboard/+layout.svelte
@@ -27,8 +27,34 @@
 </script>
 
 {#if !checkingAuth}
-<div class="topbar-wrapper flex h-18.75 shrink-0">
-<p>Notification Bar</p>
+<div class="topbar-wrapper flex h-18.75 shrink-0 justify-center items-center">
+  <div class="notification-content flex items-center gap-2.5">
+    <svg
+      class="notification-arrow-left"
+      width="25"
+      height="25"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M19 12H5M12 19l-7-7 7-7" />
+    </svg>
+    <p>Notification Bar</p>
+    <svg
+      class="notification-arrow-right"
+      width="25"
+      height="25"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke-width="2.5"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    >
+      <path d="M5 12h14M12 5l7 7-7 7" />
+    </svg>
+  </div>
 </div>
   <main class="main-content-space flex flex-col md:flex-row flex-1 min-h-0 gap-5 p-10">
     <div class="sidebar w-full md:w-64 shrink-0">
