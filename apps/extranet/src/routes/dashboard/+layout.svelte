fdc31bb8078c387a51db4a424c10226d4ffd02eb
ineszarka <zarkaines@gmail.com>
2026-09-07 15:39:29 +0200

    Transfering Main Nav to Sidebar

--- apps/extranet/src/routes/dashboard/+layout.svelte
+++ apps/extranet/src/routes/dashboard/+layout.svelte
@@ -27,40 +27,39 @@
 </script>
 
 {#if !checkingAuth}
-  <nav class="dashboard-menu flex top-0 w-full p-2.5 justify-between">
-    <p class="wrap-break-word">
-      Welcome, Welcome {user?.signInDetails?.loginId}
-    </p>
-    <ul class="menu-list flex">
-      <li><a href="/dashboard">Accueil</a></li>
-      <li><a href="/dashboard/proforma">Proforma</a></li>
-      <li><a href="/dashboard/marketing">Marketing</a></li>
-      <li><a href="/dashboard/profil">Profil</a></li>
-    </ul>
-
-    <div class="topbar-wrapper flex">
-      <div class="logo-space flex items-center">
-        <div class="logo-icon flex items-center">
-          <p class="whitespace-nowrap">au parchemin</p>
-        </div>
-      </div>
-      <div
-        class="profil-sign-out-space flex flex-wrap items-center gap-1.25 min-w-0"
-      >
-        <img
-          alt="Votre logo"
-          class="profil-picture size-8 shrink-0 rounded-full object-cover bg-gray-300"
-        />
-        <button onclick={handleSignOut} class="whitespace-nowrap"
-          >Sign out</button
-        >
-      </div>
-    </div>
-  </nav>
   <main class="main-content-space flex flex-1 min-h-0 gap-5 p-10">
     <div class="sidebar">
       <div class="sidebar-mainbar w-full h-full">
-        <p>That will be the sidebar</p>
+        <nav class="dashboard-menu flex top-0 w-full p-2.5 justify-between">
+          <p class="wrap-break-word">
+            Welcome {user?.signInDetails?.loginId}
+          </p>
+          <ul class="menu-list flex">
+            <li><a href="/dashboard">Accueil</a></li>
+            <li><a href="/dashboard/proforma">Proforma</a></li>
+            <li><a href="/dashboard/marketing">Marketing</a></li>
+            <li><a href="/dashboard/profil">Profil</a></li>
+          </ul>
+
+          <div class="topbar-wrapper flex">
+            <div class="logo-space flex items-center">
+              <div class="logo-icon flex items-center">
+                <p class="whitespace-nowrap">au parchemin</p>
+              </div>
+            </div>
+            <div
+              class="profil-sign-out-space flex flex-wrap items-center gap-1.25 min-w-0"
+            >
+              <img
+                alt="Votre logo"
+                class="profil-picture size-8 shrink-0 rounded-full object-cover bg-gray-300"
+              />
+              <button onclick={handleSignOut} class="whitespace-nowrap"
+                >Sign out</button
+              >
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
     <div class="page-content flex-1 min-w-0">
